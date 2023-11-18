16ee1686224e3a6cbbe2e63c286ea127c13edea0
Javier A. Ramírez <ja.ramirezs2@uniandes.edu.co>
2023-11-18 16:14:55 -0500

    club-socio service sin pruebas

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ClubModule } from './club/club.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClubEntity } from './club/club.entity';
 import { SocioEntity } from './socio/socio.entity';
+import { ClubSocioModule } from './club-socio/club-socio.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { SocioEntity } from './socio/socio.entity';
       synchronize: true,
       keepConnectionAlive: true,
     }),
+    ClubSocioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
