8bcaf63b45ff14918c4c003e7b9d0266a73e87d7
Javier A. Ramírez <ja.ramirezs2@uniandes.edu.co>
2023-11-18 15:22:52 -0500

    Pruebas para los servicios (menos la asociación)

--- src/club/club.service.spec.ts
+++ src/club/club.service.spec.ts
@@ -41,4 +41,123 @@ describe('ClubService', () => {
       clubesList.push(club);
     }
   };
+
+  it('findAll debe retornar todos los clubes', async () => {
+    const clubes: ClubEntity[] = await service.findAll();
+    expect(clubes).not.toBeNull();
+    expect(clubes).toHaveLength(clubesList.length);
+  });
+
+  it('findOne debe retornar un club por id', async () => {
+    const storedClub: ClubEntity = clubesList[0];
+    const club: ClubEntity = await service.findOne(storedClub.id);
+    expect(club).not.toBeNull();
+    expect(club.nombre).toEqual(storedClub.nombre);
+    expect(club.fechaFundacion).toEqual(storedClub.fechaFundacion);
+    expect(club.imagen).toEqual(storedClub.imagen);
+    expect(club.descripcion).toEqual(storedClub.descripcion);
+  });
+
+  it('findOne debe lanzar una excepción para un club inválido', async () => {
+    await expect(() => service.findOne('0')).rejects.toHaveProperty(
+      'message',
+      'El club con el id dado no existe',
+    );
+  });
+
+  it('create debe retornar un nuevo club', async () => {
+    const club: ClubEntity = {
+      id: '',
+      nombre: faker.company.name(),
+      fechaFundacion: faker.date.past(),
+      imagen: faker.image.url(),
+      descripcion: faker.lorem.sentence(),
+      socios: [],
+    };
+
+    const newClub: ClubEntity = await service.create(club);
+    expect(newClub).not.toBeNull();
+
+    const storedClub: ClubEntity = await repository.findOne({
+      where: { id: newClub.id },
+    });
+
+    expect(storedClub).not.toBeNull();
+    expect(storedClub.nombre).toEqual(newClub.nombre);
+    expect(storedClub.fechaFundacion).toEqual(newClub.fechaFundacion);
+    expect(storedClub.imagen).toEqual(newClub.imagen);
+    expect(storedClub.descripcion).toEqual(newClub.descripcion);
+  });
+
+  it('create debe lanzar una excepción para una descripción inválida', async () => {
+    const club: ClubEntity = {
+      id: '',
+      nombre: faker.company.name(),
+      fechaFundacion: faker.date.past(),
+      imagen: faker.image.url(),
+      descripcion: faker.string.sample(150),
+      socios: [],
+    };
+
+    await expect(() => service.create(club)).rejects.toHaveProperty(
+      'message',
+      'La descripción del club no puede superar los 100 caracteres',
+    );
+  });
+
+  it('update debe modificar un club', async () => {
+    const club: ClubEntity = clubesList[0];
+    club.nombre = faker.company.name();
+    club.descripcion = faker.lorem.sentence();
+    const updatedClub: ClubEntity = await service.update(club.id, club);
+    expect(updatedClub).not.toBeNull();
+    const storedClub: ClubEntity = await repository.findOne({
+      where: { id: club.id },
+    });
+    expect(storedClub).not.toBeNull();
+    expect(storedClub.nombre).toEqual(club.nombre);
+    expect(storedClub.descripcion).toEqual(club.descripcion);
+  });
+
+  it('update debe lanzar una excepción para un club inválido', async () => {
+    let club: ClubEntity = clubesList[0];
+    club = {
+      ...club,
+      nombre: faker.company.name(),
+      descripcion: faker.lorem.sentence(),
+    };
+    await expect(() => service.update('0', club)).rejects.toHaveProperty(
+      'message',
+      'El club con el id dado no existe',
+    );
+  });
+
+  it('update debe lanzar una excepción para una descripción inválida', async () => {
+    let club: ClubEntity = clubesList[0];
+    club = {
+      ...club,
+      nombre: faker.company.name(),
+      descripcion: faker.string.sample(150),
+    };
+    await expect(() => service.update(club.id, club)).rejects.toHaveProperty(
+      'message',
+      'La descripción del club no puede superar los 100 caracteres',
+    );
+  });
+
+  it('delete debe eliminar un club', async () => {
+    const club: ClubEntity = clubesList[0];
+    await service.delete(club.id);
+    const deletedClub: ClubEntity = await repository.findOne({
+      where: { id: club.id },
+    });
+    expect(deletedClub).toBeNull();
+  });
+
+  it('delete debe lanzar una excepción para un club inválido', async () => {
+    await expect(() => service.delete('0')).rejects.toHaveProperty(
+      'message',
+      'El club con el id dado no existe',
+    );
+  });
 });
--- src/socio/socio.service.spec.ts
+++ src/socio/socio.service.spec.ts
@@ -40,4 +40,119 @@ describe('SocioService', () => {
       sociosList.push(socio);
     }
   };
+
+  it('findAll debe retornar todos los socios', async () => {
+    const socios: SocioEntity[] = await service.findAll();
+    expect(socios).not.toBeNull();
+    expect(socios).toHaveLength(sociosList.length);
+  });
+
+  it('findOne debe retornar un socio por id', async () => {
+    const storedSocio: SocioEntity = sociosList[0];
+    const socio: SocioEntity = await service.findOne(storedSocio.id);
+    expect(socio).not.toBeNull();
+    expect(socio.nombreUsuario).toEqual(storedSocio.nombreUsuario);
+    expect(socio.correoElectronico).toEqual(storedSocio.correoElectronico);
+    expect(socio.fechaNacimiento).toEqual(storedSocio.fechaNacimiento);
+  });
+
+  it('findOne debe lanzar una excepción para un socio inválido', async () => {
+    await expect(() => service.findOne('0')).rejects.toHaveProperty(
+      'message',
+      'El socio con el id dado no existe',
+    );
+  });
+
+  it('create debe retornar un nuevo socio', async () => {
+    const socio: SocioEntity = {
+      id: '',
+      nombreUsuario: faker.internet.userName(),
+      correoElectronico: faker.internet.email(),
+      fechaNacimiento: faker.date.past(),
+      clubes: [],
+    };
+
+    const newSocio: SocioEntity = await service.create(socio);
+    expect(newSocio).not.toBeNull();
+
+    const storedSocio: SocioEntity = await repository.findOne({
+      where: { id: newSocio.id },
+    });
+
+    expect(storedSocio).not.toBeNull();
+    expect(storedSocio.nombreUsuario).toEqual(newSocio.nombreUsuario);
+    expect(storedSocio.correoElectronico).toEqual(newSocio.correoElectronico);
+    expect(storedSocio.fechaNacimiento).toEqual(newSocio.fechaNacimiento);
+  });
+
+  it('create debe lanzar una excepción para un correo inválido', async () => {
+    const socio: SocioEntity = {
+      id: '',
+      nombreUsuario: faker.internet.userName(),
+      correoElectronico: faker.lorem.sentence(),
+      fechaNacimiento: faker.date.past(),
+      clubes: [],
+    };
+
+    await expect(() => service.create(socio)).rejects.toHaveProperty(
+      'message',
+      'El correo electrónico del socio debe contener el caracter @',
+    );
+  });
+
+  it('update debe modificar un socio', async () => {
+    const socio: SocioEntity = sociosList[0];
+    socio.nombreUsuario = faker.internet.userName();
+    socio.correoElectronico = faker.internet.email();
+    const updatedSocio: SocioEntity = await service.update(socio.id, socio);
+    expect(updatedSocio).not.toBeNull();
+    const storedSocio: SocioEntity = await repository.findOne({
+      where: { id: socio.id },
+    });
+    expect(storedSocio).not.toBeNull();
+    expect(storedSocio.nombreUsuario).toEqual(socio.nombreUsuario);
+    expect(storedSocio.correoElectronico).toEqual(socio.correoElectronico);
+  });
+
+  it('update debe lanzar una excepción para un socio inválido', async () => {
+    let socio: SocioEntity = sociosList[0];
+    socio = {
+      ...socio,
+      nombreUsuario: faker.internet.userName(),
+      correoElectronico: faker.internet.email(),
+    };
+    await expect(() => service.update('0', socio)).rejects.toHaveProperty(
+      'message',
+      'El socio con el id dado no existe',
+    );
+  });
+
+  it('update debe lanzar una excepción para un correo inválido', async () => {
+    let socio: SocioEntity = sociosList[0];
+    socio = {
+      ...socio,
+      nombreUsuario: faker.internet.userName(),
+      correoElectronico: faker.lorem.word(),
+    };
+    await expect(() => service.update(socio.id, socio)).rejects.toHaveProperty(
+      'message',
+      'El correo electrónico del socio debe contener el caracter @',
+    );
+  });
+
+  it('delete debe eliminar un socio', async () => {
+    const socio: SocioEntity = sociosList[0];
+    await service.delete(socio.id);
+    const deletedSocio: SocioEntity = await repository.findOne({
+      where: { id: socio.id },
+    });
+    expect(deletedSocio).toBeNull();
+  });
+
+  it('delete debe lanzar una excepción para un socio inválido', async () => {
+    await expect(() => service.delete('0')).rejects.toHaveProperty(
+      'message',
+      'El socio con el id dado no existe',
+    );
+  });
 });
